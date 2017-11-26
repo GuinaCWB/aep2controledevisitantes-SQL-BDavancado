5a7be9bfbb1ed05b7aac2dc5cca43edc9bcaad7e
GuinaCWB <jr_st@hotmail.com>
2017-11-26 17:12:02 -0200

    Create Empregado.sql

---
+++ Empregado.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Empregado (
+IdentificacaoEmpregado Varchar (30) NOT NULL,
+IdTipoDeIdentificacao Numeric (30) NOT NULL,
+Nome Varchar (20) NOT NULL,
+IdEmpregado Numeric (30) NOT NULL,
+TipoServico Varchar (100) NOT NULL,
+)
