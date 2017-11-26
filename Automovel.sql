17b577f0a78caa56e3d61b469901591d68cf719b
GuinaCWB <jr_st@hotmail.com>
2017-11-26 17:10:00 -0200

    Create Automovel.sql

---
+++ Automovel.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Automovel (
+NumeroAutomovel Numeric (30) NOT NULL,
+ModeloVeiculo Varchar (50) NOT NULL,
+PlacaAutomovel Numeric (30) NOT NULL,
+NumeroVaga Numeric (30) NOT NULL,
+UNIQUE (NumeroAutomovel);
+PRIMARY KEY (PlacaAutomovell);
+)
