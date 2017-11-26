1e888f2b88d233574d1d9f0c90b0e8ed90a6fc67
GuinaCWB <jr_st@hotmail.com>
2017-11-26 17:11:02 -0200

    Create Prestador de Servicos.sql

---
+++ Prestador de Servicos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Prestador de Servicos (
+IdentificacaoPrestadorDeServicos Varchar (30) NOT NULL,
+IdTipoDeIdentificacao Numeric (30) NOT NULL,
+IdPrestadorDeServicos Numeric (30) NOT NULL,
+Nome Varchar (20) NOT NULL,
+TipoServico Varchar (100) NOT NULL
+)
